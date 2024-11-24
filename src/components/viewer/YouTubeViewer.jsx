6e2c32b41a7651dd528fe414baf2c045a40e6d07
i-am-sergio <sergio15.dev@gmail.com>
2024-11-24 12:57:35 -0500

    frontend: add loading state and spinner to YouTubeViewer component

--- src/components/viewer/YouTubeViewer.jsx
+++ src/components/viewer/YouTubeViewer.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const YouTubeViewer = ({ videoUrl }) => {
+  const [isLoading, setIsLoading] = useState(true); // Estado para controlar si el video está cargando
   let embedUrl;
 
   // Verifica si el enlace es una lista de reproducción o un video individual
@@ -16,6 +17,11 @@ export const YouTubeViewer = ({ videoUrl }) => {
     embedUrl = videoUrl;
   }
 
+  // Función que se llama cuando el iframe se ha cargado
+  const handleLoad = () => {
+    setIsLoading(false); // El video ha cargado, ocultamos el logo de carga
+  };
+
   return (
     <div className="w-full h-full flex justify-center items-center bg-transparent">
       <div
@@ -27,6 +33,32 @@ export const YouTubeViewer = ({ videoUrl }) => {
           alignItems: "center",
         }}
       >
+        {isLoading && (
+          <div className="absolute flex justify-center items-center w-full h-full bg-transparent py-2 pt-2 bg-opacity-75">
+            {/* Aquí puedes colocar cualquier spinner o logo de carga */}
+            <svg
+              className="animate-spin h-16 w-16 text-gray-600"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <circle
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+                className="opacity-25"
+              ></circle>
+              <path
+                fill="currentColor"
+                d="M4 12a8 8 0 0116 0"
+                className="opacity-75"
+              ></path>
+            </svg>
+          </div>
+        )}
         <iframe
           width="100%" // Se ajusta al contenedor
           height="100%" // Se ajusta al contenedor
@@ -35,6 +67,7 @@ export const YouTubeViewer = ({ videoUrl }) => {
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
           title="YouTube Content"
+          onLoad={handleLoad} // Llama a handleLoad cuando se carga el iframe
         ></iframe>
       </div>
     </div>

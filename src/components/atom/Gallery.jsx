2b270533299e41a91818d96868afb55fc6c9f693
i-am-sergio <sergio15.dev@gmail.com>
2024-11-24 15:16:23 -0500

    frontend: enhance Gallery component with improved zoom scale and add floating ID display

--- src/components/atom/Gallery.jsx
+++ src/components/atom/Gallery.jsx
@@ -64,7 +64,7 @@ export const Gallery = () => {
                     top: '50%',
                     left: '50%',
                     transformOrigin: 'center',
-                    transform: 'translate(-50%, -50%) scale(1.5)',
+                    transform: 'translate(-50%, -50%) scale(1.8)',
                     zIndex: 50,
                   }
                 : {}
@@ -75,13 +75,22 @@ export const Gallery = () => {
               alt={photo.alt}
               className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105"
             />
+            {/* Ajusta el div para que se ponga encima de la imagen de manera flotante a lado izq superior */}
+            <div
+            // alinea verticalmente tambien
+              className='absolute text-center top-1 left-1 bg-black bg-opacity-60 text-white w-8 h-8 rounded-full
+              flex justify-center items-center text-xs font-bold
+              ' // Ajusta el color de fondo y el tamaño del texto
+            >
+              {photo.id}
+            </div>
             {zoomedId === photo.id && (
               <button
                 onClick={handleClose}
-                className="absolute top-2 right-2 bg-black bg-opacity-60 text-white rounded-full p-2"
+                className="absolute top-1 right-1 bg-black bg-opacity-60 text-white w-8 h-8 rounded-full p-2 flex justify-center items-center"
                 style={{ zIndex: 60 }}
               >
-                <FaTimes size={20} />
+                <FaTimes size={15} />
               </button>
             )}
           </div>
